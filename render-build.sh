d303124d3bce40fb8994520b8fc43ace2168b823
reyor69693-ship-it <reyor69693@gmail.com>
2025-08-24 06:24:58 -0500

    Delete render-build.sh

--- render-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-# Instala las dependencias del sistema operativo para el monitor virtual
-apt-get update && apt-get install -y xvfb chromium-browser
-
-# Instala las dependencias de Python
-pip install -r requirements.txt
